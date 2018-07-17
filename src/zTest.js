94f2742cfe368727b2b44dc1fa87eecf2ed1faf2
Daniel Swid <da.webs@gmail.com>
2018-07-16 17:10:25 -0700

    Added reference to index.js
    * thats all
    *

--- src/zTest.js
+++ src/zTest.js
@@ -10,8 +10,13 @@ async function sdk()
 {
   let sdk = await Swarm.create('https://api-stage.swarm.fund');
 
-  document.write( sdk.horizon.server.transactions().forAccount()
+  //document.write( sdk.horizon.server.transactions().forAccount() );
 
+  let page = await sdk.horizon.balances.getPage();
+  console.log('Page', page.data);
+
+  let prevPage = await page.fetchPrev()
+  console.log('Previous page', prevPage.data);
 
   //let page = await sdk.horizon.balances.getPage();
   return sdk;
