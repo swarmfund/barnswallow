d6c3d6f70fb61714aaeb22e02b422119873c6dd5
Daniel Swid <daniel@swarm.fund>
2018-07-18 16:10:44 -0700

    Babel and Webpack updates
    * trying to get react working
    * works on cloud foundry
    
    [Issue] na
    [Test] y
    [Review] na

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,44 @@
 const path = require('path')
 const webpack = require('webpack')
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const htmlWebpackPlugin = new HtmlWebpackPlugin({  // Also generate a test.html
+  title: "Swarm Barnswallow",
+  template: path.join(__dirname, "src/assets/test.html"),
+  filename: "./test.html"
+});
 
 module.exports = {
-  entry: './src/index.js',
+  entry: path.join(__dirname, 'src/index2.js'),
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: "babel-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js'
   },
   plugins: [
+    new HtmlWebpackPlugin(), // Generates default index.html
+    htmlWebpackPlugin,
     // Ignore native modules
     new webpack.IgnorePlugin(/ed25519/)
-  ]
+  ],
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
+  devServer: {
+    port: 3001
+  }
+
 };
 
 /*
