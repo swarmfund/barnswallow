f1d06054d7bcc445af947b84dbb10fa9520030eb
Daniel Swid <daniel@swarm.fund>
2018-07-31 06:06:03 -0700

    Remove axios middleware from index.js
    * can be brought back later if desired
    * no need for this right now
    
    [Issue] na
    [Test] n
    [Review] na

--- src/index.js
+++ src/index.js
@@ -1,28 +1,16 @@
 import React from 'react';
 import {render} from 'react-dom';
 import { Provider, connect } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import axios from 'axios';
-//import axiosMiddleware from 'redux-axios-middleware';
 import configureStore from './store/configureStore'
 import App from './containers/App';
 import './index.css';
 
-const client = axios.create({
-  baseURL: 'https://api.github.com',
-  responseType: 'json'
-});
-
-
 
 const store = configureStore();
-
 //store.dispatch();
 
 console.log(store.getState());
 
-//const store = createStore(postReducer);
 render(
     <Provider store={store}>
       <App />
