b7c01beff10ca7a8e8476cd05da32321e03c9392
Daniel Swid <daniel@swarm.fund>
2018-10-08 15:16:18 -0700

    Remove unused selectors (commented out)
    [Issue] na
    [Test] y
    [Review] na

--- src/components/ledger/index.js
+++ src/components/ledger/index.js
@@ -3,6 +3,5 @@ import * as actions from './actions';
 import * as LedgerList from './LedgerList';
 import * as constants from './constants';
 import ledgers from './reducer';
-//import * as selectors from './selectors';
 
 export default { actions, LedgerList, constants, ledgers };
--- src/components/transactions/index.js
+++ src/components/transactions/index.js
@@ -2,6 +2,5 @@ import * as actions from './actions';
 import * as TransactionList from './TransactionList';
 import * as constants from './constants';
 import transactions from './reducer';
-//import * as selectors from './selectors';
 
 export default { actions, TransactionList, constants, transactions };
